19506caac14d552a2d2e23e0a07cb401a55df80b
Rick Behl <rickbehl@hotmail.com>
2017-08-23 17:00:06 +0800

    Changed name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello f00k1n W4nk4!");
+    response.end("Hello Clement!");
 
 });
 
